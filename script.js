c074011a34b7ae2dd191c90f2354c6467c4f309f
Igor Dias Fernandes <86981851+IgorDF02@users.noreply.github.com>
2024-03-03 16:31:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,30 @@
 	function logoff(){
 		window.location.replace('logoff-session.php');
 	}
+
+
+	//register.html
+	document.getElementById('user_registration').addEventListener('input', function (event) {
+		// Obtém o valor atual do campo de entrada
+		let inputValue = event.target.value;
+
+		// Remove qualquer caractere que não seja um número
+		inputValue = inputValue.replace(/\D/g, '');
+
+		// Define o valor do campo de entrada como somente números
+		event.target.value = inputValue;
+	});
+	
+	//register.php
+	var password = document.getElementById("user_password"), confirm = document.getElementById("user_confirm");
+
+	function validatePassword(){
+  		if(password.value != confirm.value) {
+    	confirm.setCustomValidity("Passwords Don't Match");
+  			} else {
+    	confirm.setCustomValidity('');
+  		}
+	}
+
+		password.onchange = validatePassword;
+		confirm.onkeyup = validatePassword;
