6a5127a23b6c3e5a002258294169fe061c2363b7
Hemily Maria <78569901+marialy19@users.noreply.github.com>
2024-03-11 18:00:08 -0300

    add: Validação de matrícula e e-mail

--- register-session.php
+++ register-session.php
@@ -8,6 +8,25 @@ $email = mysqli_real_escape_string($conn, $_POST['user_email']);
 $senha = mysqli_real_escape_string($conn, $_POST['user_password']);
 $confirmacao = mysqli_real_escape_string($conn, $_POST['user_confirm']);
 
+$result_of_login_check = mysqli_query ($conn, "SELECT id FROM registro WHERE user_registration='". $matricula ."'");
+			
+			if($result_of_login_check->num_rows != 0){
+				$erro = true;
+				echo "<script type='text/javascript'>
+						alert('Esta matrícula já está cadastrada.');
+						window.location.replace('register.html');
+					</script>";
+			}
+
+$result_of_login_check = mysqli_query ($conn, "SELECT id FROM registro WHERE user_email='". $email ."'");
+			
+			if($result_of_login_check->num_rows != 0){
+				$erro = true;
+				echo "<script type='text/javascript'>
+						alert('Este e-mail já está cadastrado.');
+						window.location.replace('register.html');
+					</script>";
+			}
 
 $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
 
@@ -18,6 +37,7 @@ $stmt->bind_param("ssss", $nome, $matricula, $email, $senhaHash);
 if ($stmt->execute()) {
     echo "Registro inserido com sucesso";
     header("Refresh: 3; url=index.html");
+    exit();
 } else {
     echo "Erro: " . $stmt->error;
 }
