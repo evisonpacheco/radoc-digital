cce3d7e4095de60d084c785f45b45d52bb908757
Hemily Maria <78569901+marialy19@users.noreply.github.com>
2024-03-11 13:39:03 -0300

    Mudança de Usuário para E-mail

--- login-session.php
+++ login-session.php
@@ -4,47 +4,37 @@ require 'db-connection.php';
 
 session_start();
 
-if (empty($_POST['user_login'])) {
-            echo "<script type='text/javascript'>
-			alert('Preencha o campo Usuário.');
-			window.location.replace('index.html');
-   		  </script>";
-        } elseif (empty($_POST['user_password'])) {
-            echo "<script type='text/javascript'>
-			alert('Preencha o campo Senha.');
-			window.location.replace('index.html');
-		  </script>";
-        } elseif (!empty($_POST['user_login']) && !empty($_POST['user_password'])) {
+if (!empty($_POST['user_email']) && !empty($_POST['user_password'])) {
 			
-			$usuario = $_POST['user_login'];
-			$senha = $_POST['user_password'];
+	$email = $_POST['user_email'];
+	$senha = $_POST['user_password'];
 
-			$result_of_login_check = mysqli_query($conn, "SELECT nome, user_registration, user_password
+	$result_of_login_check = mysqli_query($conn, "SELECT user_name, user_registration, user_password
 								      FROM registro 
-								      WHERE user_login = '" . $usuario . "' ");
+								      WHERE user_email = '" . $email . "' ");
 				
-			if ($result_of_login_check->num_rows == 1) {
+	if ($result_of_login_check->num_rows == 1) {
 
-			$result_row = $result_of_login_check->fetch_object();
+		$result_row = $result_of_login_check->fetch_object();
 	
-				if (password_verify($senha, $result_row->user_password)) {
+		if (password_verify($senha, $result_row->user_password)) {
 		
-					$_SESSION['user_name'] = $result_row->nome;
-					$_SESSION['user_registration'] = $result_row->user_registration;
-					header("Location: pages/home.html");
-					exit();
+			$_SESSION['user_name'] = $result_row->user_name;
+			$_SESSION['user_registration'] = $result_row->user_registration;
+			header("Location: pages/home.html");
+			exit();
 						
-				} else {
-					echo "<script type='text/javascript'>
-							alert('Senha incorreta.');
-							window.location.replace('index.html');
-						  </script>";
+			} else {
+				echo "<script type='text/javascript'>
+					alert('Senha incorreta.');
+					window.location.replace('index.html');
+					</script>";
 				  }
-			}	else {
-					echo "<script type='text/javascript'>
-							alert('Usuário incorreto ou não cadastrado.');
-							window.location.replace('index.html');
-						  </script>";
+			} else {
+				echo "<script type='text/javascript'>
+					alert('Usuário incorreto ou não cadastrado.');
+					window.location.replace('index.html');
+					</script>";
 				}
 		  }
 
