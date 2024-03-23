4ca2d39378f6a98557a6fcc333df77a55139da10
Evison <evison.pacheco21@gmail.com>
2024-03-22 22:41:51 -0300

    Update: Iniciando 'Single Page Application' com JavaScript

---
+++ assets/scripts/singlepage.js
@@ -0,0 +1,9 @@
+function setEnsinoPage() {
+  document.getElementById("homepage__single").style.display = "none";
+  document.getElementById("ensinopage__single").style.display = "block";
+}
+
+function setHomePage() {
+  document.getElementById("homepage__single").style.display = "block";
+  document.getElementById("ensinopage__single").style.display = "none";
+}
