6503cf14a11ed4aca1537eff8dd8731a9b1efc9a
Evison <evison.pacheco21@gmail.com>
2024-04-16 17:55:13 -0300

    Style: Colocando estilização na página 'Extensão.php'

--- pages/extensão.php
+++ pages/extensão.php
@@ -3,6 +3,7 @@ include 'login_verification.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,18 +11,20 @@ include 'login_verification.php';
   <link rel="stylesheet" href="../assets/styles/style.css">
   <script src="../script.js" defer></script>
 </head>
-<body class="bodyhome">
-  <header class="headerhome">
-    <img class="headerhome__image" src="../assets/images/login/logoufra.svg" alt="Logo da UFRA">
-    <p class="headerhome__title">UFRA - Universidade Federal Rural da Amazônia</p>
+
+<body class="bodyextensao">
+  <header class="headerextensao">
+    <img class="headerextensao__image" src="../assets/images/login/logoufra.svg" alt="Logo da UFRA">
+    <p class="headerextensao__title">UFRA - Universidade Federal Rural da Amazônia</p>
   </header>
-  <main class="mainhome">
+  <main class="mainextensao">
     <div class="sidemenu">
       <div class="sidemenu__title">MENU PRINCIPAL</div>
       <div class="sidemenu__flex">
         <ul class="sidemenu__list">
-          <a href="home.php" class="sidemenu__element">Página Inicial</a>
-          <a href="./editar-dados.php" class="sidemenu__element" style="text-decoration:none;color:inherit;" >Editar dados</a>
+          <a href="./home.php" class="sidemenu__element">Página Inicial</a>
+          <a href="./editar-dados.php" class="sidemenu__element">Editar
+            dados</a>
           <a href="./orientacao.php" class="sidemenu__element">Orientação</a>
           <a href="./projeto.php" class="sidemenu__element">Projeto</a>
           <a href="./extensao.php" class="sidemenu__element">Extensão</a>
@@ -31,36 +34,36 @@ include 'login_verification.php';
           <a href="./administrativa.php" class="sidemenu__element">Administrativo</a>
           <a href="./pagina-adm.php" class="sidemenu__element">Página Administrativa</a>
         </ul>
-        <div id="sair" class="sidemenu__logout"><a style="text-decoration:none;color:#F7F7F7;" href="../logoff-session.php">SAIR</a></div>
+        <div id="sair" class="sidemenu__logout"><a href="../logoff-session.php">SAIR</a></div>
       </div>
     </div>
-    <div class="homepage">
-      <div class="homepage__info">
-        <h2 class="homepage__title">Atividades de Extensão</h2>
+    <div class="extensaopage">
+      <div class="extensaopage__info">
+        <h2 class="extensaopage__title">Atividades de Extensão</h2>
       </div>
 
       <div class="extensao">
-        <form action="../pages-back/pages.php" method="post">
+        <form class="extensaoform" action="../pages-back/pages.php" method="post">
         <input type="hidden" name="formulario" value="5">
-          <table>
-            <caption>Extensão</caption>
+          <table class="extensaotable">
+            <caption class="extensaotable__title">Extensão</caption>
             <tr>
               <th>Nª do Doc</th>
               <th>Projetos</th>
             </tr>
             <tr>
-              <td><input type="text" name="ext_doc1" id="ext_doc1"></td>
+              <td><input class="extensaotable__input" type="text" name="ext_doc1" id="ext_doc1"></td>
               <td>
-              <input type="text" name="ext_titulo1" placeholder="Título 1">
+                <input class="extensaotable__input" type="text" name="ext_titulo1" placeholder="Título 1">
               </td>
             </tr>
 
             <tr>
             <td></td>
               <td>
-              <input type="radio" name="ext_leader_project1" id="ext_coordenador1">
+                <input class="extensaotable__input" type="radio" name="ext_leader_project1" id="ext_coordenador1">
               <label for="orientador1">Coordenador</label>
-              <input type="radio" name="ext_leader_project1" id="ext_colaborador1">
+                <input class="extensaotable__input" type="radio" name="ext_leader_project1" id="ext_colaborador1">
               <label for="coordenador1">Colaborador</label>
               </td>
             </tr>
@@ -70,65 +73,68 @@ include 'login_verification.php';
               <th>Projetos</th>
             </tr>
             <tr>
-              <td><input type="text" name="ext_doc2" id="ext_doc2"></td>
+              <td><input class="extensaotable__input" type="text" name="ext_doc2" id="ext_doc2"></td>
               <td>
-              <input type="text" name="ext_titulo2" placeholder="Título 2">
+                <input class="extensaotable__input" type="text" name="ext_titulo2" placeholder="Título 2">
               </td>
             </tr>
             <tr>
               <td></td>
               <td>
-              <input type="radio" name="ext_leader_project2" id="ext_coordenador2">
+                <input class="extensaotable__input" type="radio" name="ext_leader_project2" id="ext_coordenador2">
               <label for="orientador2">Coordenador</label>
-              <input type="radio" name="ext_leader_project2" id="ext_colaborador2">
+                <input class="extensaotable__input" type="radio" name="ext_leader_project2" id="ext_colaborador2">
               <label for="coordenador2">Colaborador</label>
               </td>
             </tr>
           </table>
 
-          <table>
-            <caption>Estágios de Extensão</caption>
+          <table class="extensaotable">
+            <caption class="extensaotable__title">Estágios de Extensão</caption>
             <tr>
               <th>Nª do Doc</th>
               <th>Área de conhecimento</th>
               <th>Instituição/Local</th>
+            </tr>
+            <tr>
+              <td><input type="text" name="ext_estagio_doc" id="ext_estagio_doc"></td>
+              <td><input class="extensaotable__input" type="text" name="ext_conhecimento" id="ext_conhecimento"></td>
+              <td><input class="extensaotable__input" type="text" name="ext_instituicao" id="ext_instituicao"></td>
+            </tr>
+            <tr class="extensaotable__row">
               <th>Período</th>
               <th>Carga horária semanal</th>
             </tr>
-            <tr>
-            	<td><input type="text" name="ext_estagio_doc" id="ext_estagio_doc"></td>
-                <td><input type="text" name="ext_conhecimento" id="ext_conhecimento"></td>
-                <td><input type="text" name="ext_instituicao" id="ext_instituicao"></td>
-                <td><input type="text" name="ext_periodo" id="ext_periodo"></td>
-                <td><input type="number" name="ext_chs" id="ext_chs"></td>
+            <tr class="extensaotable__row">
+              <td><input class="extensaotable__input" type="text" name="ext_periodo" id="ext_periodo"></td>
+              <td><input class="extensaotable__input" type="number" name="ext_chs" id="ext_chs"></td>
             </tr>
           </table>
           
-           <table>
-            <caption>Atividade de ensino não-formal</caption>
+          <table class="extensaotable">
+            <caption class="extensaotable__title">Atividade de ensino não-formal</caption>
             <tr>
               <th>Nª do Doc</th>
               <th>Atividades</th>
               <th>Carga Horária Semanal</th>
             </tr>
             <tr>
-            	<td><input type="text" name="ext_enf_doc" id="ext_enf_doc"></td>
-          		<td><input type="text" name="ext_atividade" id="ext_atividade"></td>
-                <td><input type="number" name="ext_chs_total" id="ext_chs_total" placeholder="CH total / 23 semanas"></td>
+              <td><input class="extensaotable__input" type="text" name="ext_enf_doc" id="ext_enf_doc"></td>
+              <td><input class="extensaotable__input" type="text" name="ext_atividade" id="ext_atividade"></td>
+              <td><input class="extensaotable__input" type="number" name="ext_chs_total" id="ext_chs_total"
+                  placeholder="CH total / 23 semanas"></td>
             </tr>
             </table>
             
-            <div>
-              <nav>
-                <input type="button" value="Novo">
-                <input class="save-button" type="submit" value="Salvar">
-                <input type="button" value="Excluir">
+          <nav class="navigation">
+            <input class="navigation__button" type="submit" value="Salvar">
+            <input class="navigation__button navigation__button--reset" type="reset" value="Resetar">
               </nav>
-            </div>
         </form>
           
       </div>
     </div>
     </main>
 </body>
+
 </html>
