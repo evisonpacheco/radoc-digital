f21398bef534a8652928fa93b778e6eec8535305
Evison <evison.pacheco21@gmail.com>
2024-04-16 23:04:52 -0300

    Style: Adicionando estilização na página 'Administrativa.php'

--- pages/administrativa.php
+++ pages/administrativa.php
@@ -3,6 +3,7 @@ include 'login_verification.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,18 +11,20 @@ include 'login_verification.php';
   <link rel="stylesheet" href="../assets/styles/style.css">
   <script src="../script.js" defer></script>
 </head>
-<body class="bodyhome">
-  <header class="headerhome">
-    <img class="headerhome__image" src="../assets/images/login/logoufra.svg" alt="Logo da UFRA">
-    <p class="headerhome__title">UFRA - Universidade Federal Rural da Amazônia</p>
+
+<body class="bodyadministrativa">
+  <header class="headeradministrativa">
+    <img class="headeradministrativa__image" src="../assets/images/login/logoufra.svg" alt="Logo da UFRA">
+    <p class="headeradministrativa__title">UFRA - Universidade Federal Rural da Amazônia</p>
   </header>
-  <main class="mainhome">
+  <main class="mainadministrativa">
     <div class="sidemenu">
       <div class="sidemenu__title">MENU PRINCIPAL</div>
       <div class="sidemenu__flex">
         <ul class="sidemenu__list">
-          <a href="home.php" class="sidemenu__element">Página Inicial</a>
-		      <a href="./editar-dados.php" class="sidemenu__element" style="text-decoration:none;color:inherit;" >Editar dados</a>
+          <a href="./home.php" class="sidemenu__element">Página Inicial</a>
+          <a href="./editar-dados.php" class="sidemenu__element">Editar
+            dados</a>
           <a href="./orientacao.php" class="sidemenu__element">Orientação</a>
           <a href="./projeto.php" class="sidemenu__element">Projeto</a>
           <a href="./extensao.php" class="sidemenu__element">Extensão</a>
@@ -31,17 +34,17 @@ include 'login_verification.php';
           <a href="./administrativa.php" class="sidemenu__element">Administrativo</a>
 	  <a href="./pagina-adm.php" class="sidemenu__element">Página Administrativa</a>
         </ul>
-        <div id="sair" class="sidemenu__logout"><a style="text-decoration:none;color:#F7F7F7;" href="../logoff-session.php">SAIR</a></div>
+        <div id="sair" class="sidemenu__logout"><a class="sidemenu__logout--button"
+            href="../logoff-session.php">SAIR</a></div>
       </div>
     </div>
-    <div class="homepage">
-      <div class="homepage__info">
-        <h2 class="homepage__title">Administrativa</h2>
+    <div class="administrativapage">
+      <div class="administrativapage__info">
+        <h2 class="administrativapage__title">Administrativa</h2>
       </div>
-
-            <form action="../pages-back/pages.php" method="post">
+      <form class="administrativaform" action="../pages-back/pages.php" method="post">
             <input type="hidden" name="formulario" value="9">
-              <div>
+        <div class="administrativaform__element">
                 <label for="adm_table">Tabela: </label>
                 <input type="text" name="adm_table" id="adm_table">
                 <br>
@@ -57,20 +60,13 @@ include 'login_verification.php';
                 <label for="adm_description">Descrição: </label>
                 <textarea name="adm_description" id="adm_description" rows="4"></textarea>
               </div>
-
-              <div>
-              <nav>
-                <input type="button" value="Novo">
-                <input class="save-button" type="submit" value="Salvar">
-                <input type="button" value="Excluir">
+        <nav class="navigation">
+          <input class="navigation__button" type="submit" value="Salvar">
+          <input class="navigation__button navigation__button--reset"type="reset" value="Resetar">
               </nav>
-            </div>
-
             </form>
           </div>
-
-      </div>
-    </div>
   </main>
 </body>
+
 </html>
