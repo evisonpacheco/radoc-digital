b9b0ac18499c97c455650457bfdd1c5d0e4ebbe7
Evison <evison.pacheco21@gmail.com>
2024-04-16 22:38:08 -0300

    Style: Adicionando estilização na página 'Qualificação.php'

--- pages/qualificação.php
+++ pages/qualificação.php
@@ -3,32 +3,15 @@ include 'login_verification.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Qualificação - UFRA</title>
   <link rel="stylesheet" href="../assets/styles/style.css">
   <script src="../script.js" defer></script>
-  <style>
-    table {
-      border-collapse: collapse;
-      width: 100%;
-    }
-    th, td {
-      border: 1px solid #dddddd;
-      text-align: left;
-      padding: 8px;
-    }
-    th {
-      background-color: #f2f2f2;
-    }
-    .description-textarea {
-      width: 100%;
-      height: 100px; /* Defina a altura inicial aqui */
-      resize: vertical; /* Permite expandir verticalmente */
-    }
-  </style>
 </head>
+
 <body class="bodyqualificacao">
   <header class="headerqualificacao">
     <img class="headerqualificacao__image" src="../assets/images/login/logoufra.svg" alt="Logo da UFRA">
@@ -36,11 +19,12 @@ include 'login_verification.php';
   </header>
   <main class="mainqualificacao">
     <div class="sidemenu">
-      <div class="sidemenu__title">Qualificação</div>
+      <div class="sidemenu__title">MENU PRINCIPAL</div>
       <div class="sidemenu__flex">
         <ul class="sidemenu__list">
-          <a href="home.php" class="sidemenu__element">Página Inicial</a>
-          <a href="./editar-dados.php" class="sidemenu__element" style="text-decoration:none;color:inherit;">Editar dados</a>
+          <a href="./home.php" class="sidemenu__element">Página Inicial</a>
+          <a href="./editar-dados.php" class="sidemenu__element">Editar
+            dados</a>
           <a href="./orientacao.php" class="sidemenu__element">Orientação</a>
           <a href="./projeto.php" class="sidemenu__element">Projeto</a>
           <a href="./extensao.php" class="sidemenu__element">Extensão</a>
@@ -50,61 +34,35 @@ include 'login_verification.php';
           <a href="./administrativa.php" class="sidemenu__element">Administrativo</a>
           <a href="./pagina-adm.php" class="sidemenu__element">Página Administrativa</a>
         </ul>
-        <div id="sair" class="sidemenu__logout"><a style="text-decoration:none;color:#F7F7F7;" href="../logoff-session.php">SAIR</a></div>
+        <div id="sair" class="sidemenu__logout"><a class="sidemenu__logout--button"
+            href="../logoff-session.php">SAIR</a></div>
       </div>
     </div>
     <div class="qualificacaopage">
       <div class="qualificacaopage__info">
         <h2 class="qualificacaopage__title">Qualificação</h2>
-        <p>Sistema de Cadastro de Atividades Docentes.</p>
-        <hr>
+      </div>
        
-        <form action="../pages-back/pages.php" method="post">
+      <form class="qualificacaoform" action="../pages-back/pages.php" method="post">
+        <p class="qualificacaoform__title">Sistema de Cadastro de Atividades Docentes.</p>
         <input type="hidden" name="formulario" value="7">
-        <!-- <table>
-          <thead>
-            <tr>
-              <th></th>
-              <th>Data Início</th>
-              <th>Data Término</th>
-              <th>Tabela</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><input type="checkbox"></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table> 
-       
-        <hr> -->
-       
-        <div>
+        <div class="qualificacaoform__element">
           <label>Data Início:</label>
-          <input type="date" name="qua_data_inicio" id="qua_data_inicio">
+          <input class="qualificacaoform__input" type="date" name="qua_data_inicio" id="qua_data_inicio">
           <label>Data Término:</label>
-          <input type="date" name="qua_data_termino" id="qua_data_termino">
+          <input class="qualificacaoform__input" type="date" name="qua_data_termino" id="qua_data_termino">
         </div>
-        <div>
+        <div class="qualificacaoform__element qualificacaoform__element--textarea">
           <label>Descrição:</label>
           <textarea class="description-textarea" name="qua_descricao" id="qua_descricao"></textarea>
         </div>
-
-            <div>
-              <nav>
-                <input type="button" value="Novo">
-                <input class="save-button" type="submit" value="Salvar">
-                <input type="button" value="Excluir">
+        <nav class="navigation">
+          <input class="navigation__button" type="submit" value="Salvar">
+          <input class="navigation__button navigation__button--reset" type="reset" value="Resetar">
               </nav>
-            </div>
         </form>
       </div>
-    </div>
   </main>
-
-
 </body>
+
 </html>
